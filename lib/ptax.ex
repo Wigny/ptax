512c0b360ad338f0c2cfbdc34efada8851c06d8a
Wígny Almeida <wignybora@gmail.com>
2021-12-21 12:53:35 +0000

    Initial commit

---
+++ lib/ptax.ex
@@ -0,0 +1,18 @@
+defmodule PTAX do
+  @moduledoc """
+  Documentation for `PTAX`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PTAX.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ptax_test.exs
@@ -0,0 +1,8 @@
+defmodule PTAXTest do
+  use ExUnit.Case
+  doctest PTAX
+
+  test "greets the world" do
+    assert PTAX.hello() == :world
+  end
+end
