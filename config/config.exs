f7307b6c3c473a7f44b47a7d82eafbd80d04c5fa
Wígny Almeida <wignybora@gmail.com>
2022-01-02 03:02:09 +0000

    Add Hackney options

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,8 @@
 import Config
 
-tesla_adapter = if config_env() != :test, do: Tesla.Adapter.Hackney, else: Tesla.Mock
+tesla_adapter =
+  if config_env() != :test,
+    do: {Tesla.Adapter.Hackney, ssl_options: [verify: :verify_none]},
+    else: Tesla.Mock
 
 config :tesla, adapter: tesla_adapter
