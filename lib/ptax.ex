4e3bbd6b5bb665e32e2a6ad262ab5f6a38cec8d8
Wígny Almeida <wignybora@gmail.com>
2022-02-25 19:52:42 +0000

    Update types

--- lib/ptax.ex
+++ lib/ptax.ex
@@ -9,15 +9,15 @@ defmodule PTAX do
   @type moeda :: atom()
   @type operacao :: :compra | :venda
 
-  @type converter_opts ::
-          Conversor.opts()
-          | [
-              de: moeda,
-              para: moeda,
-              data: Date.t() | nil,
-              operacao: operacao | nil,
-              boletim: Cotacao.Boletim.t() | nil
-            ]
+  @typep converter_opts ::
+           Conversor.opts()
+           | [
+               de: moeda,
+               para: moeda,
+               data: Date.t() | nil,
+               operacao: operacao | nil,
+               boletim: Cotacao.Boletim.t() | nil
+             ]
 
   @spec moedas :: {:ok, list(Moeda.t())} | {:error, Error.t()}
   defdelegate moedas, to: Moeda, as: :list
@@ -30,11 +30,7 @@ defmodule PTAX do
       iex> PTAX.converter(5, de: :USD, para: :BRL, data: ~D[2021-12-24], operacao: :compra, boletim: PTAX.Cotacao.Boletim.Fechamento)
       {:ok, #Decimal<28.2705>}
   """
-  @spec converter(
-          valor :: valor,
-          opts :: converter_opts
-        ) :: {:ok, Decimal.t()} | {:error, Error.t()}
-
+  @spec converter(valor :: valor, opts :: converter_opts) :: {:ok, valor} | {:error, Error.t()}
   def converter(valor, opts) when is_list(opts) do
     default_opts = %{
       data: "America/Sao_Paulo" |> Timex.now() |> Timex.to_date(),
@@ -58,7 +54,7 @@ defmodule PTAX do
       iex> PTAX.converter!(5, de: :USD, para: :BRL, data: ~D[2021-12-24], operacao: :compra, boletim: PTAX.Cotacao.Boletim.Fechamento)
       #Decimal<28.2705>
   """
-  @spec converter!(valor :: valor, opts :: converter_opts) :: Decimal.t()
+  @spec converter!(valor :: valor, opts :: converter_opts) :: valor
   def converter!(valor, opts) do
     case converter(valor, opts) do
       {:ok, result} -> result
