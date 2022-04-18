ea0e2d1d85a873fda10f2e935f700bad70de596d
Wígny Almeida <wignybora@gmail.com>
2022-04-18 17:14:21 +0000

    Fix formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 # Used by "mix format"
 [
   locals_without_parens: [value: 2],
-  import_deps: ~w[tesla typed_struct enum_type]a,
+  import_deps: ~w[tesla typed_struct]a,
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
